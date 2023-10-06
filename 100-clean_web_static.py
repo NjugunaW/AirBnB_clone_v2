34024a858b6982dd67970dd3d7ba1a25a67d58b5
NjugunaW <njugunawangui10@gmail.com>
2023-10-05 18:39:15 -0700

    Corrections

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,28 +1,86 @@
 #!/usr/bin/python3
 # A Fabric script that deletes out-of-date archives, using the function do_clean 
 import os
-from fabric.api import *
+from fabric.api import env, local, put, run
 
+env.user = 'ubuntu'
 env.hosts = ["34.224.2.52", "54.173.45.189"]
 
 
-def do_clean(number=0):
-    """Delete out-of-date archives.
-    Args:
-        number: The number of archives to keep.
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
+def do_pack():
     """
-    number = 1 if int(number) == 0 else int(number)
+    This function generates an archive from the web static content
+    Returns:
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+    """
+    pres_mmt = datetime.now().strftime("%Y%m%d%H%M%S")
+    file = "versions/web_static_{}.tgz".format(pres_mmt)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
 
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives
+
+def do_deploy(archive_path):
+    """
+    This function distributes archive to servers
+    Args:
+        archive_path:
+
+    Returns:
+
+    """
+    try:
+        archive = archive_path.split('/')[-1]
+        path = '/data/web_static/releases/' + archive.strip('.tgz')
+        current = '/data/web_static/current'
+        put(archive_path, '/tmp')
+        run('mkdir -p {}/'.format(path))
+        run('tar -xzf /tmp/{} -C {}'.format(archive, path))
+        run('rm /tmp/{}'.format(archive))
+        run('mv {}/web_static/* {}'.format(path, path))
+        run('rm -rf {}/web_static'.format(path))
+        run('rm -rf {}'.format(current))
+        run('ln -s {} {}'.format(path, current))
+        print('New version deployed!')
+        return True
+    except FileNotFoundError:
+        return False
+
+
+def deploy():
+    """
+    This function creates and distributes archive
+    Returns:
+
+    """
+    archive_path = do_pack()
+    answer = do_deploy(archive_path)
+    return answer
+
+
+def do_clean(number=0):
+    """
+    This function deletes out-of-date archives
+    Args:
+        number:
+
+    Returns:
+
+    """
+    if number == 0 or number == 1:
+        with lcd('./versions/'):
+            local("ls -lv | rev | cut -f 1 | rev | \
+                head -n +1 | xargs -d '\n' rm -rf")
+        with cd('/data/web_static/releases/'):
+            run("sudo ls -lv | rev | cut -f 1 | \
+                rev | head -n +1 | xargs -d '\n' rm -rf")
+    else:
+        with lcd('./versions/'):
+            local("ls -lv | rev | cut -f 1 | rev | \
+                head -n +{} | xargs -d '\n' rm -rf".format(number))
+        with cd('/data/web_static/releases/'):
+            run("sudo ls -lv | rev | cut -f 1 | \
+                rev | head -n +{} | xargs -d '\n' rm -rf".format(number))
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-# A Fabric script that creates and distributes an archive to your web servers, using the function deploy
-
+# A Fabric script that creates and distributes an archive
 import os.path
 from datetime import datetime
-from fabric.api import env
-from fabric.api import local
-from fabric.api import put
-from fabric.api import run
+from fabric.api import env, local, put, run
 
+env.user = 'ubuntu'
 env.hosts = ["34.224.2.52", "54.173.45.189"]
 
 
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
+    """
+    This function creates and distributes an archive
+    Returns:
+
+    """
+    pres_mmt = datetime.now().strftime("%Y%m%d%H%M%S")
+    file = "versions/web_static_{}.tgz".format(pres_mmt)
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
@@ -29,48 +25,38 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
     """
-    if os.path.isfile(archive_path) is False:
-        return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+    This function distributes an archive to servers
+    Args:
+        archive_path:
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    Returns:
+
+    """
+    try:
+        archive = archive_path.split('/')[-1]
+        path = '/data/web_static/releases/' + archive.strip('.tgz')
+        current = '/data/web_static/current'
+        put(archive_path, '/tmp')
+        run('mkdir -p {}/'.format(path))
+        run('tar -xzf /tmp/{} -C {}'.format(archive, path))
+        run('rm /tmp/{}'.format(archive))
+        run('mv {}/web_static/* {}'.format(path, path))
+        run('rm -rf {}/web_static'.format(path))
+        run('rm -rf {}'.format(current))
+        run('ln -s {} {}'.format(path, current))
+        print('New version deployed!')
+        return True
+    except FileNotFoundError:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
 
 
 def deploy():
-    """Create and distribute an archive to a web server."""
-    file = do_pack()
-    if file is None:
-        return False
-    return do_deploy(file)
+    """
+    This function creates and distributes an archive to the web servers
+    Returns:
+
+    """
+    archive_path = do_pack()
+    answer = do_deploy(archive_path)
+    return answer
